072aa9a98c055745a361dd8ec19c80a8bf921025
Manel <ea3eiz@ea3eiz.com>
2023-04-21 13:14:34 +0200

    1

--- menu_extra_05.sh
+++ menu_extra_05.sh
@@ -39,11 +39,15 @@ clear
                         #sh /home/orangepi/ORANGEPI/ejecutar_ImagenActualizada.sh &                    
                         cd /home/orangepi/ORANGEPI3
                         
-                        git pull 
+                        git pull
+
+                        sudo rm -R /home/orangepi/ORANGEPI
                                                 
-                        cp /home/orangepi/ORANGEPI3/* /home/orangepi/ORANGEPI
+                        cp -a /home/orangepi/ORANGEPI3/ /home/orangepi/ORANGEPI
                                                 
-                        cp -R /home/orangepi/ORANGEPI/qt/ /home/orangepi/
+                        sudo rm -R /home/orangepi/qt
+                        
+                        cp -a /home/orangepi/ORANGEPI/qt/ /home/orangepi/
 
                         chmod 777 -R /home/orangepi/qt
 			break;;
