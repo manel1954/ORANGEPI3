09936bf22f7ca40a7a3f55f878c2cdca406ef5eb
Manel <ea3eiz@ea3eiz.com>
2023-04-24 12:57:20 +0200

    Update crear_zip.sh

--- crear_zip.sh
+++ crear_zip.sh
@@ -1,55 +1,89 @@
 ﻿#!/bin/bash
 
+            cd /home/orangepi
+            
+            sudo rm /home/orangepi/COPIA_SEGURIDAD/*.*
+            sudo chmod +x -R /home/orangepi/COPIA_SEGURIDAD
             cd /home/orangepi/MMDVMHost
-            sudo cp -f TODOS_LOS_INIS.ini /home/orangepi/Downloads
-            sudo cp -f MMDVM.ini /home/orangepi/Downloads
-            sudo cp -f MMDVM.ini_copia /home/orangepi/Downloads
-            sudo cp -f MMDVM.ini_copia2 /home/orangepi/Downloads
-            sudo cp -f MMDVM.ini_copia3 /home/orangepi/Downloads
-            sudo cp -f MMDVM.ini_original /home/orangepi/Downloads
-            sudo cp -f MMDVMBM.ini /home/orangepi/Downloads
-            sudo cp -f MMDVMBM.ini_copia /home/orangepi/Downloads
-            sudo cp -f MMDVMBM.ini_copia2 /home/orangepi/Downloads
-            sudo cp -f MMDVMBM.ini_copia3 /home/orangepi/Downloads
-            sudo cp -f MMDVMLIBRE.ini /home/orangepi/Downloads
-            sudo cp -f MMDVMLIBRE.ini_uno /home/orangepi/Downloads
-            sudo cp -f MMDVMLIBRE.ini_dos /home/orangepi/Downloads      
-            sudo cp -f MMDVMPLUS.ini /home/orangepi/Downloads
-            sudo cp -f MMDVMPLUS.ini_copia /home/orangepi/Downloads
-            sudo cp -f MMDVMPLUS.ini_copia2 /home/orangepi/Downloads
-            sudo cp -f MMDVMPLUS.ini_copia3 /home/orangepi/Downloads
+            cp -f TODOS_LOS_INIS.ini /home/orangepi/COPIA_SEGURIDAD
+            cp -f MMDVM.ini /home/orangepi/COPIA_SEGURIDAD
+            cp -f MMDVM.ini_copia /home/orangepi/COPIA_SEGURIDAD
+            cp -f MMDVM.ini_copia2 /home/orangepi/COPIA_SEGURIDAD
+            cp -f MMDVM.ini_copia3 /home/orangepi/COPIA_SEGURIDAD
+            cp -f MMDVM.ini_original /home/orangepi/COPIA_SEGURIDAD
+            cp -f MMDVMBM.ini /home/orangepi/COPIA_SEGURIDAD
+            cp -f MMDVMBM.ini_copia /home/orangepi/COPIA_SEGURIDAD
+            cp -f MMDVMBM.ini_copia2 /home/orangepi/COPIA_SEGURIDAD
+            cp -f MMDVMBM.ini_copia3 /home/orangepi/COPIA_SEGURIDAD
+            cp -f MMDVMESPECIAL.ini /home/orangepi/COPIA_SEGURIDAD
+            cp -f MMDVMESPECIAL.ini_copia /home/orangepi/COPIA_SEGURIDAD
+            cp -f MMDVMESPECIAL.ini_copia2 /home/orangepi/COPIA_SEGURIDAD   
+            cp -f MMDVMESPECIAL.ini_copia3 /home/orangepi/COPIA_SEGURIDAD   
+            cp -f MMDVMPLUS.ini /home/orangepi/COPIA_SEGURIDAD
+            cp -f MMDVMPLUS.ini_copia /home/orangepi/COPIA_SEGURIDAD
+            cp -f MMDVMPLUS.ini_copia2 /home/orangepi/COPIA_SEGURIDAD
+            cp -f MMDVMPLUS.ini_copia3 /home/orangepi/COPIA_SEGURIDAD
+
+            cp -f MMDVMDMR2NXDN.ini /home/orangepi/COPIA_SEGURIDAD
+            cp -f MMDVMDMR2YSF.ini /home/orangepi/COPIA_SEGURIDAD
+
+            cp -f /home/orangepi/DMR2NXDN/DMR2NXDN.ini  /home/orangepi/COPIA_SEGURIDAD
+            cp -f /home/orangepi/NXDNClients/NXDNGateway/NXDNGateway.ini  /home/orangepi/COPIA_SEGURIDAD
+            cp -f /home/orangepi/DMR2YSF/DMR2YSF.ini  /home/orangepi/COPIA_SEGURIDAD
 
             #solo Dstar y solo Fusion
-            cp -f MMDVMDSTAR.ini /home/orangepi/Downloads
-            cp -f MMDVMFUSION.ini /home/orangepi/Downloads
-            #fin  Dstar y solo Fusion
+            cp -f MMDVMDSTAR.ini /home/orangepi/COPIA_SEGURIDAD
+            cp -f MMDVMFUSION.ini /home/orangepi/COPIA_SEGURIDAD
+            #fin  Dstar y solo Fusion 
 
             cd  /home/orangepi/YSF2DMR
-            cp -f YSF2DMR.ini /home/orangepi/Downloads
-            cp -f YSF2DMR.ini_copia_01 /home/orangepi/Downloads
-            cp -f YSF2DMR.ini_copia_02 /home/orangepi/Downloads
-            cp -f YSF2DMR.ini_copia_03 /home/orangepi/Downloads
-            cp -f YSF2DMR.ini_copia_04 /home/orangepi/Downloads
+            cp -f YSF2DMR.ini /home/orangepi/COPIA_SEGURIDAD
+            cp -f YSF2DMR.ini_copia_01 /home/orangepi/COPIA_SEGURIDAD
+            cp -f YSF2DMR.ini_copia_02 /home/orangepi/COPIA_SEGURIDAD
+            cp -f YSF2DMR.ini_copia_03 /home/orangepi/COPIA_SEGURIDAD
+            cp -f YSF2DMR.ini_copia_04 /home/orangepi/COPIA_SEGURIDAD
+
+            cd  /home/orangepi/DMR2YSF
+            cp -f TG-YSFList.txt /home/orangepi/COPIA_SEGURIDAD
 
             cd  /home/orangepi/YSFClients/YSFGateway/
-            sudo cp -f YSFGateway.ini /home/orangepi/Downloads
+            cp -f YSFGateway.ini /home/orangepi/COPIA_SEGURIDAD
+
+            cd  /home/orangepi/NXDNClients/NXDNGateway/
+            cp -f NXDNGateway.ini /home/orangepi/COPIA_SEGURIDAD
+
+            cd  /home/orangepi/NXDNClients/NXDNGateway/
+            cp -f NXDNHosts.txt NXDNGateway.ini /home/orangepi/COPIA_SEGURIDAD
             
-            cd /home/orangepi/bluedv/
-            sudo cp -f BlueDVconfig.ini /home/orangepi/Downloads
+            # cd /home/orangepi/bluedv/
+            # cp -f BlueDVconfig.ini /home/orangepi/COPIA_SEGURIDAD 
 
             cd /usr/local/etc/svxlink/
-            sudo cp -f svxlink.conf /home/orangepi/Downloads
+            cp -f svxlink.conf /home/orangepi/COPIA_SEGURIDAD
 
             cd /usr/local/etc/svxlink/svxlink.d/
-            sudo cp -f ModuleEchoLink.conf /home/orangepi/Downloads
+            cp -f ModuleEchoLink.conf /home/orangepi/COPIA_SEGURIDAD
 
-            sudo cp /usr/local/etc/opendv/ircddbgateway /home/orangepi/Downloads
-            sudo cp /usr/local/etc/opendv/dstarrepeater /home/orangepi/Downloads
+            sudo cp /usr/local/etc/opendv/ircddbgateway /home/orangepi/COPIA_SEGURIDAD
+            sudo cp /usr/local/etc/opendv/dstarrepeater /home/orangepi/COPIA_SEGURIDAD
 
             cd /home/orangepi
-            sudo cp info_panel_control.ini /home/orangepi/Downloads
+            cp info_panel_control.ini /home/orangepi/COPIA_SEGURIDAD
+
+            cd /home/orangepi/COPIA_SEGURIDAD
+            sudo chmod 777 -R /home/orangepi/COPIA_SEGURIDAD
+            
+            tar -zcvf copia.tar.gz *
+            #mv copia.tar.gz /home/orangepi/COPIA_SEGURIDAD
+                      
+            #cd /home/orangepi/COPIA_SEGURIDAD
+            sudo rm *ini*
+            sudo rm *.txt
+            sudo rm *.conf
+            sudo rm *dstar*
+            sudo rm *ircddb*
 
 #cd /var/www/html/upload/files
 cd /home/orangepi/
-tar -zcvf copia.tar.gz Downloads
+tar -zcvf copia.tar.gz
 cp  copia.tar.gz /var/www/html/upload/files
