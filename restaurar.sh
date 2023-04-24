6b64f2b87e3983d792b15034de9e21c52bb5e02d
Manel <ea3eiz@ea3eiz.com>
2023-04-24 14:20:01 +0200

    Update restaurar.sh

--- restaurar.sh
+++ restaurar.sh
@@ -15,6 +15,12 @@ echo "*      DEL MMDVMHOST QUE TIENES ACTUALIZADO       *"
 echo "***************************************************"
             sudo chmod -R 777 /home/orangepi/Downloads
             cd /home/orangepi/Downloads
+
+
+
+
+
+
             sudo cp -f TODOS_LOS_INIS.ini /home/orangepi/MMDVMHost
             sudo cp -f MMDVM.ini /home/orangepi/MMDVMHost
             sudo cp -f MMDVM.ini_copia /home/orangepi/MMDVMHost
@@ -25,25 +31,27 @@ echo "***************************************************"
             sudo cp -f MMDVMBM.ini_copia /home/orangepi/MMDVMHost
             sudo cp -f MMDVMBM.ini_copia2 /home/orangepi/MMDVMHost
             sudo cp -f MMDVMBM.ini_copia3 /home/orangepi/MMDVMHost
-            sudo cp -f MMDVMLIBRE.ini /home/orangepi/MMDVMHost
-            sudo cp -f MMDVMLIBRE.ini_uno /home/orangepi/MMDVMHost
-            sudo cp -f MMDVMLIBRE.ini_dos /home/orangepi/DoMMDVMHost     
+            sudo cp -f MMDVMESPECIAL.ini /home/orangepi/MMDVMHost
+            sudo cp -f MMDVMESPECIAL.ini_copia /home/orangepi/MMDVMHost
+            sudo cp -f MMDVMESPECIAL.ini_copia2 /home/orangepi/MMDVMHost
+            sudo cp -f MMDVMESPECIAL.ini_copia3 /home/orangepi/MMDVMHost    
             sudo cp -f MMDVMPLUS.ini /home/orangepi/MMDVMHost
             sudo cp -f MMDVMPLUS.ini_copia /home/orangepi/MMDVMHost
             sudo cp -f MMDVMPLUS.ini_copia2 /home/orangepi/MMDVMHost
             sudo cp -f MMDVMPLUS.ini_copia3 /home/orangepi/MMDVMHost
+            
+            cp -f MMDVMDMR2NXDN.ini /home/orangepi/MMDVMHost
+            cp -f MMDVMDMR2YSF.ini /home/orangepi/MMDVMHost
 
-            #solo Dstar
+            cp -f DMR2NXDN.ini  /home/orangepi/DMR2NXDN
+            cp -f NXDNGateway.ini /home/orangepi/NXDNClients/NXDNGateway
+            cp -f DMR2YSF.ini /home/orangepi/DMR2YSF
+            
+            #solo Dstar y solo Fusion
             sudo cp -f MMDVMDSTAR.ini /home/orangepi/MMDVMHost
-            sudo cp -f MMDVMDSTAR.ini_copia /home/orangepi/MMDVMHost
-            sudo cp -f MMDVMDSTAR.ini_copia2 /home/orangepi/MMDVMHost
-            sudo cp -f MMDVMDSTAR.ini_copia3 /home/orangepi/MMDVMHost
-
-            #solo Fusion
             sudo cp -f MMDVMFUSION.ini /home/orangepi/MMDVMHost
-            sudo cp -f MMDVMFUSION.ini_copia /home/orangepi/MMDVMHost
-            sudo cp -f MMDVMFUSION.ini_copia2 /home/orangepi/MMDVMHost
-            sudo cp -f MMDVMFUSION.ini_copia3 /home/orangepi/MMDVMHost
+            #fin  Dstar y solo Fusion
+
 
             cp -f YSF2DMR.ini /home/orangepi/YSF2DMR
             cp -f YSF2DMR.ini_copia_01 /home/orangepi/YSF2DMR
@@ -51,24 +59,25 @@ echo "***************************************************"
             cp -f YSF2DMR.ini_copia_03 /home/orangepi/YSF2DMR
             cp -f YSF2DMR.ini_copia_04 /home/orangepi/YSF2DMR
             
+            
             cp -f TG-YSFList.txt /home/orangepi/DMR2YSF
-                      
-            cp -f MMDVMDMR2NXDN.ini /home/orangepi/MMDVMHost
-            cp -f MMDVMDMR2YSF.ini /home/orangepi/MMDVMHost
-
-            cp -f DMR2NXDN.ini  /home/orangepi/DMR2NXDN
-            cp -f NXDNGateway.ini /home/orangepi/NXDNClients/NXDNGateway
-            cp -f DMR2YSF.ini /home/orangepi/DMR2YSF
-
             sudo cp -f YSFGateway.ini /home/orangepi/YSFClients/YSFGateway/
 
-            sudo cp -f BlueDVconfig.ini /home/orangepi/bluedv/
+         
+            cp -f NXDNGateway.ini /home/orangepi/NXDNClients/NXDNGateway/        
+            cp -f NXDNHosts.txt NXDNGateway.ini /home/orangepi/NXDNClients/NXDNGateway/
+
+            # sudo cp -f BlueDVconfig.ini /home/orangepi/bluedv/
+
 
             sudo cp -f svxlink.conf /usr/local/etc/svxlink/
-            
             sudo cp -f ModuleEchoLink.conf /usr/local/etc/svxlink/svxlink.d/
+            
+            
+            
+            
+            sudo cp ircddbgateway /usr/local/etc/opendv/
+            sudo cp dstarrepeater /usr/local/etc/opendv/
 
-            cp ircddbgateway /usr/local/etc/opendv/
-            cp dstarrepeater /usr/local/etc/opendv/
 
             cp info_panel_control.ini /home/orangepi/          		
