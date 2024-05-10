be3796f4947e03889811ec2cc7c948adf770a024
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-05-10 15:19:45 +0200

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash	                               
                                            
-                        cd /home/orangepi/
+                        cd /home/orangepi/ORANGEPI3/
                         
                         git pull
                         
