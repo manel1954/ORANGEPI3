b8e7077ba85d1aa847446f615ae695083c97fda5
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-05-31 10:28:42 +0200

    1

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -4,15 +4,14 @@
                         
                         git pull --force
 
-                        #cd /home/orangepi/AUTOSTART
-                        #git pull --force
+                        
 
                         sudo rm -R /home/orangepi/ORANGEPI
 
                         mkdir /home/orangepi/ORANGEPI
                                                 
                         cp -R /home/orangepi/ORANGEPI3/* /home/orangepi/ORANGEPI
-                                                
+                        cp -R /home/orangepi/ORANGEPI3/AUTOSTART/* /home/orangepi/AUTOSTART                      
                         
                         sudo rm -R /home/orangepi/qt
 
@@ -22,3 +21,5 @@
                         sudo chmod 777 -R /home/orangepi/ORANGEPI
                         
                         sudo chmod 777 -R /home/orangepi/qt
+
+                        sudo chmod 777 -R /home/orangepi/AUTOSTART
