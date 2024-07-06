0687fe665d3c457df8f58ab0733b886f1069786c
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-07-06 15:56:29 +0200

    Update actualizar_dxtra_reflectores.sh

--- actualizar_dxtra_reflectores.sh
+++ actualizar_dxtra_reflectores.sh
@@ -1,33 +1,7 @@
 #!/bin/bash
-ROJO="\033[1;31m"
-VERDE="\033[1;32m"
-BLANCO="\033[1;37m"
-AMARILLO="\033[1;33m"
-CIAN="\033[1;36m"
-GRIS="\033[0m"
-echo "${VERDE}"
 
-                                ejecutar1=S
-                                case $ejecutar1 in
-                                [sS]* ) echo ""
                                 cd /usr/local/share/opendv/
                                 sudo curl --fail -o DExtra_Hosts.txt -s http://www.arrg.us/HF/DExtra_Hosts.txt
-                                sleep 2
-                                echo "   ********************************************************************"
-                                echo "   *                ACTUALIZANDO REFLECTORES DEXTRA                   *"
-                                echo "   ********************************************************************"
-                                sleep 3
-                                clear
-                                echo "${AMARILLO}"
-                                echo "   ********************************************************************"
-                                echo "   *                  PROCESO EFECTUADO CORRECTAMENTE                 *"
-                                echo "   ********************************************************************"
-                                sleep 3
-                                break;;
-                                [nN]* ) echo ""
-                                clear
-                                exit;
-                                break;;
-                                esac﻿
+﻿
                      
 
