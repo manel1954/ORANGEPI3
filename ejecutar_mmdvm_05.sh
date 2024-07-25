079ae20224579961b81b10b6fab0d98385a8fb5b
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-07-25 10:09:47 +0200

    Update ejecutar_mmdvm_05.sh

--- ejecutar_mmdvm_05.sh
+++ ejecutar_mmdvm_05.sh
@@ -18,8 +18,8 @@ cd /home/orangepi/MMDVMHost
 
 xterm -geometry 86x16+1277+803 -bg blue -fg white -fa 'roboto' -fs 9x -T RADIO -e sudo ./MMDVMRADIO MMDVM.ini
 
-sed -i "6c Exec=sh ejecutar_mmdvm_05.sh" /home/orangepi/Desktop/MMDVM.desktop
-sed -i "7c Icon=/home/orangepi/ORANGEPI/MMDVM.png" /home/orangepi/Desktop/MMDVM.desktop
-sed -i "4c Name[es_ES]=Abrir Radio" /home/orangepi/Desktop/MMDVM.desktop
+#sed -i "6c Exec=sh ejecutar_mmdvm_05.sh" /home/orangepi/Desktop/MMDVM.desktop
+#sed -i "7c Icon=/home/orangepi/ORANGEPI/MMDVM.png" /home/orangepi/Desktop/MMDVM.desktop
+#sed -i "4c Name[es_ES]=Abrir Radio" /home/orangepi/Desktop/MMDVM.desktop
 
-sed -i "5c MMDVM=OFF" /home/orangepi/status.ini
+#sed -i "5c MMDVM=OFF" /home/orangepi/status.ini
