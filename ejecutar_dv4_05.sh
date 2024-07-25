1e87e158335d7590733cd809587aaa38a9882b64
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-07-25 11:06:57 +0200

    Update ejecutar_dv4_05.sh

--- ejecutar_dv4_05.sh
+++ ejecutar_dv4_05.sh
@@ -8,7 +8,7 @@ sed -i "4c DV4mini=ON" /home/orangepi/status.ini
 
 cd /home/orangepi/dv4mini
 
-su root -c 'mono dv4mini.exe'
+sudo mono dv4mini.exe
 
 
 sed -i "6c Exec=sh ejecutar_dv4_05.sh" /home/orangepi/Desktop/DV4mini.desktop
