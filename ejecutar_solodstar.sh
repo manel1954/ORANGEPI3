bdaee958f314ee580e41688f91bb45e1c2e782cc
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-08-07 12:32:13 +0200

    Update ejecutar_solodstar.sh

--- ejecutar_solodstar.sh
+++ ejecutar_solodstar.sh
@@ -19,9 +19,9 @@ xterm -geometry 86x16+$x+803 -bg black -fg green -fa 'roboto' -fs 9x -T DSTAR -e
 cd /home/orangepi/ORANGEPI
 sh ejecutar_d-star_05.sh
 
-sed -i "6c Exec=sh ejecutar_solodstar.sh" /home/orangepi/Desktop/AbrirsoloDstar.desktop
-sed -i "7c Icon=/home/orangepi/ORANGEPI/SOLO_D-STAR.png" /home/orangepi/Desktop/AbrirsoloDstar.desktop
-sed -i "11c Name[es_ES]=Abrir solo D-STAR" /home/orangepi/Desktop/AbrirsoloDstar.desktop
-sed -i "13c SOLODSTAR=OFF" /home/orangepi/status.ini
+# sed -i "6c Exec=sh ejecutar_solodstar.sh" /home/orangepi/Desktop/AbrirsoloDstar.desktop
+# sed -i "7c Icon=/home/orangepi/ORANGEPI/SOLO_D-STAR.png" /home/orangepi/Desktop/AbrirsoloDstar.desktop
+# sed -i "11c Name[es_ES]=Abrir solo D-STAR" /home/orangepi/Desktop/AbrirsoloDstar.desktop
+# sed -i "13c SOLODSTAR=OFF" /home/orangepi/status.ini
 
  
