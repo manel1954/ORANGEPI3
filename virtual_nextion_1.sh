5996bdb6d99f8a30c1ca4fbe661aa2ea4a948210
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-11-14 18:35:35 +0100

    1

--- virtual_nextion_1.sh
+++ virtual_nextion_1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # sudo killall socat  
-cd /home/pi/A108
+cd /home/orangepi/ORANGEPI
 sudo socat -d -d pty,link=/dev/virtual1,raw,echo=0 pty,link=/dev/virtual10,raw,echo=0 &
 
 #sudo socat -d -d PTY,raw,echo=0,b9600,link=/dev/virtual1 PTY,raw,echo=0,b9600,link=/dev/virtual2
--- virtual_nextion_2.sh
+++ virtual_nextion_2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # sudo killall socat  
-cd /home/pi/A108
+cd /home/orangepi/ORANGEPI
 sudo socat -d -d pty,link=/dev/virtual2,raw,echo=0 pty,link=/dev/virtual20,raw,echo=0 &
 
 #sudo socat -d -d PTY,raw,echo=0,b9600,link=/dev/virtual1 PTY,raw,echo=0,b9600,link=/dev/virtual2
