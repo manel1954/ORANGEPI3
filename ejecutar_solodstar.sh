66c0137c10959b7e24d00b240c30413e37cb11ff
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-06-11 23:54:11 +0200

    1

--- ejecutar_solodstar.sh
+++ ejecutar_solodstar.sh
@@ -12,12 +12,15 @@ sed -i "11c Name[es_ES]=Cerrar solo D-STAR" /home/orangepi/Desktop/AbrirsoloDsta
 
 sed -i "13c SOLODSTAR=ON" /home/orangepi/status.ini
 
+x=$(awk "NR==95" /home/orangepi/status.ini)
+
 cd /home/orangepi/MMDVMHost
 echo "{$VERDE}"
 
-sudo ./MMDVMDSTAR MMDVMDSTAR.ini & 
-cd /home/orangepi/ORANGEPI
-sh ejecutar_d-star_05.sh
+xterm -geometry 86x16+$x+803 -bg black -fg yellow -fa 'roboto' -fs 9x -T SOLODSTAR -e sudo ./MMDVMDSTAR MMDVMDSTAR.ini & 
+ircddbgateway
+
+x=$(awk "NR==95" /home/pi/status.ini)
 
 sed -i "6c Exec=sh ejecutar_solodstar.sh" /home/orangepi/Desktop/AbrirsoloDstar.desktop
 sed -i "7c Icon=/home/orangepi/ORANGEPI/SOLO_D-STAR.png" /home/orangepi/Desktop/AbrirsoloDstar.desktop
