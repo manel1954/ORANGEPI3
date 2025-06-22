5f4ac3adf23294986303583a43ca36419bb20d56
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-06-22 21:41:26 +0200

    Update actualizar_IDS_MMDVMHost_05.sh

--- actualizar_IDS_MMDVMHost_05.sh
+++ actualizar_IDS_MMDVMHost_05.sh
@@ -6,19 +6,19 @@ echo "\33[1;32m   **************************************************************
 echo "\33[1;32m   ******************** ACTUALIZANDO IDS INDICATIVOS ******************"
 echo "\33[1;32m   ********************************************************************"
 
-#sudo sh /home/orangepi/MMDVMHost/linux/DMRIDUpdate.sh
+sudo sh /home/orangepi/MMDVMHost/linux/DMRIDUpdate.sh
 #
-#cp /home/orangepi/MMDVMHost/DMRIds.dat /home/orangepi/DMR2NXDN
-#cp /home/orangepi/MMDVMHost/DMRIds.dat /home/orangepi/DMR2YSF
-#cp /home/orangepi/MMDVMHost/DMRIds.dat /home/orangepi/YSF2DMR
+cp /home/orangepi/MMDVMHost/DMRIds.dat /home/orangepi/DMR2NXDN
+cp /home/orangepi/MMDVMHost/DMRIds.dat /home/orangepi/DMR2YSF
+cp /home/orangepi/MMDVMHost/DMRIds.dat /home/orangepi/YSF2DMR
 
 
 # Cambio realizado el 22-06-2025 para actualizar los IDS en MMDVMHost
 
-cd /home/orangepi/MMDVMHost
-sudo curl --fail -o DMRIds.dat -s http://www.pistar.uk/downloads/DMRIds.dat
-sudo chmod 777 /home/orangepi/MMDVMHost/DMRIds.dat
-
-cp /home/orangepi/MMDVMHost/DMRIds.dat /home/orangepi/DMR2NXDN
-cp /home/orangepi/MMDVMHost/DMRIds.dat /home/orangepi/DMR2YSF
-cp /home/orangepi/MMDVMHost/DMRIds.dat /home/orangepi/YSF2DMR
+#cd /home/orangepi/MMDVMHost
+#sudo curl --fail -o DMRIds.dat -s http://www.pistar.uk/downloads/DMRIds.dat
+#sudo chmod 777 /home/orangepi/MMDVMHost/DMRIds.dat
+#
+#cp /home/orangepi/MMDVMHost/DMRIds.dat /home/orangepi/DMR2NXDN
+#cp /home/orangepi/MMDVMHost/DMRIds.dat /home/orangepi/DMR2YSF
+#cp /home/orangepi/MMDVMHost/DMRIds.dat /home/orangepi/YSF2DMR
