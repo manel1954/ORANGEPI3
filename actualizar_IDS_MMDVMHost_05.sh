f759b8c0a21185db3f5d2b27945a7e25891acacf
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-06-22 22:02:46 +0200

    1

--- actualizar_IDS_MMDVMHost_05.sh
+++ actualizar_IDS_MMDVMHost_05.sh
@@ -1,38 +1,10 @@
 ﻿#!/bin/bash
 
-
-
 clear
 echo " "
 echo "\33[1;32m   ********************************************************************"
 echo "\33[1;32m   ******************** ACTUALIZANDO IDS INDICATIVOS ******************"
 echo "\33[1;32m   ********************************************************************"
-#sleep 10
-#var=`grep -n -m 1 '\<DMRIDPATH\>' /home/orangepi/MMDVMHost/linux/DMRIDUpdate.sh`
-#buscar=":"
-#largo_linea=`expr index $var $buscar`
-#largo_linea=`expr $largo_linea - 1`
-#numero_linea=`expr substr $var 1 $largo_linea`
-#letrac=c
-#linea_sed=$numero_linea$letrac
-#echo "$linea_sed"
-#read a
-#sed -i "$linea_sed DMRIDPATH=/home/orangepi/MMDVMHost" /home/orangepi/MMDVMHost/linux/DMRIDUpdate.sh
-#
-## DMR IDs now served by RadioID.net 
-#var=`grep -n -m 1 '\<DATABASEURL\>' /home/orangepi/MMDVMHost/linux/DMRIDUpdate.sh`
-#buscar=":"
-#largo_linea=`expr index $var $buscar`
-#largo_linea=`expr $largo_linea - 1`
-#numero_linea=`expr substr $var 1 $largo_linea`
-#letrac=c
-#linea_sed=$numero_linea$letrac
-#
-#
-#echo "$linea_sed" 
-#read a
-#
-#sed -i "$linea_sed DATABASEURL='https://ham-digital.org/status/users.csv'" /home/orangepi/MMDVMHost/linux/DMRIDUpdate.sh
 
 sudo sh /home/orangepi/MMDVMHost/linux/DMRIDUpdate.sh
 
